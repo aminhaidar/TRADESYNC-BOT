3e94a3703e165bcad9a9902e25ac3c0bbfe7ddfa
Amin Haidar <amin.haidar@nghmacpvxp2qc2j.mynetworksettings.com>
2025-03-10 20:52:46 -0500

    Add Gunicorn via waitress for production

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
